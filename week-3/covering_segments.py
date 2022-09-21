5b819b34bf0554c8b1aa482d8f2813c20c73ebaa
İlker M. Sıtkı <ilkermeliksitki@gmail.com>
2022-09-21 21:27:53 +0300

    most right -> rightmost variable change done

--- week-3/covering_segments.py
+++ week-3/covering_segments.py
@@ -9,17 +9,17 @@ for i in range(n):
 # list is sorted according to the second element of inner array.
 coordinates_ls.sort(key=lambda x: x[1])
 
-# most right is the selection points of the segments.
-most_right = coordinates_ls[0][1]
-point_ls = [most_right]
+# rightmost is the selection points of the segments.
+rightmost = coordinates_ls[0][1]
+point_ls = [rightmost]
 count = 1
 for i in range(n):
-    # if most right point is not in the stated range, meaning that
+    # if rightmost point is not in the stated range, meaning that
     # that line segment is not selected, so we select it by updating
-    # most right value.
-    if not (coordinates_ls[i][0]) <= most_right <= (coordinates_ls[i][1]):
-        most_right = coordinates_ls[i][1]
-        point_ls.append(most_right)
+    # rightmost value.
+    if not (coordinates_ls[i][0]) <= rightmost <= (coordinates_ls[i][1]):
+        rightmost = coordinates_ls[i][1]
+        point_ls.append(rightmost)
         count += 1
 
 print(count)
